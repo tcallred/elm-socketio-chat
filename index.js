7a7c9a58a97ae3ac849d837ff0b702d05681d3db
tcallred <taylor.allred21@gmail.com>
2021-01-09 14:37:30 -0700

    working single room chat server

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+const http = require('http').createServer(app)
+const path = require('path')
+const io = require('socket.io')(http)
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/client/index.html'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+// TODO Make this better
+app.get('/elm.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/elm.js'))
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+})
+
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
